5ce032a50e00f2d2cd41e6e3ba718c6f7d9bde75
Onni Hakala <onni@seravo.fi>
2016-02-15 23:19:18 +0200

    Change /DATA to /data in all config files

--- files/run.sh
+++ files/run.sh
@@ -2,17 +2,17 @@
 
 [ -f /run-pre.sh ] && /run-pre.sh
 
-if [ ! -d /DATA/htdocs ] ; then
-  mkdir -p /DATA/htdocs
-  chown nginx:www-data /DATA/htdocs
+if [ ! -d /data/htdocs ] ; then
+  mkdir -p /data/htdocs
+  chown nginx:www-data /data/htdocs
 fi
 
 # start php-fpm
-mkdir -p /DATA/logs/php-fpm
+mkdir -p /data/logs/php-fpm
 php-fpm
 
 # start nginx
-mkdir -p /DATA/logs/nginx
+mkdir -p /data/logs/nginx
 mkdir -p /tmp/nginx
 chown nginx /tmp/nginx
 nginx
